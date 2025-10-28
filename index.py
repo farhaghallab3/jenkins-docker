729bb6cb7f45f624b75b590a8cf9d875f45d8ac6
farhaghallab3 <132375082+farhaghallab3@users.noreply.github.com>
2025-10-28 18:20:00 +0300

    fine

--- index.py
+++ index.py
@@ -8,3 +8,8 @@ def divide(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero.")
     return x / y
+print(sum(10, 5))
+print(multiply(10, 5))
+print(subtract(10, 5))
+print(divide(10, 5))
+print(divide(10, 0))  # This will raise an exception    
